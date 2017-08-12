2ba720be30a2e9acacb39bc91b5abb71534b2197
Shaistha Seema <nayeemashariff26@gmail.com>
2017-08-12 10:16:31 +0530

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,58 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Staff Analysis</title>
+	
+	<link rel="stylesheet" type="text/css" href="st.css">
+	<link href='https://fonts.googleapis.com/css?family=Amaranth' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css?family=Architects Daughter' rel='stylesheet'>
+	
+	<style type="text/css">
+		h1,h2 
+		  {
+    		font-family: 'Amaranth';
+    		font-size: 34px;
+          }
+        h3
+          {
+			font-family: 'Architects Daughter';
+			font-size: 25px;
+          }
+    </style>
+	
+</head>
+<body>
+	
+	<img id="logo" src="4.jpg">
+	<img id="dnv" src="5.png">	
+	
+	<h1> JEPPIAAR ENGINEERING COLLEGE </h1>
+	<h2> STAFF ANALYSIS PROCESS </h2>
+	<img src="2.png">
+
+        
+	<div id="main-wrapper">
+	<center>
+		<h3>Login </h3>
+		<img src="2.jpg" class="avatar">
+	</center>
+      <form class="myform" action="index.php" method="post">
+ 		<label><b>Username Type :</b></label><br>
+ 		<input type="text" class="inputvalues" placeholder="Type your Username Type"/><br>
+ 		<label><b>Username Name :</b></label><br>
+ 		<input type="text" class="inputvalues" placeholder="Type your Username Name"/><br>
+ 		<label><b>Password :</b></label><br>
+ 		<input type="password" class="inputvalues" placeholder="Type your password"/><br>
+ 		<input type="button" id="login_btn" value="Login"/><br>
+ 	  </form>
+ 		</div>	
+ 		<div id="footer">
+ 			<h4>Copyrights &copy;2016</h4>
+ 		</div>
+ 		
+</body>
+</html>
